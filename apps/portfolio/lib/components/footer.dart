80b98f7e620f478fccf9a3c3a6839f3e7e097299
박현우 <ghwhsbsb123@gmail.com>
2025-11-03 02:51:26 +0900

    chore: update dependencies across the project

--- apps/portfolio/lib/components/footer.dart
+++ apps/portfolio/lib/components/footer.dart
@@ -6,19 +6,9 @@ class Footer extends StatelessComponent {
   @override
   Component build(BuildContext context) {
     return footer(
-      classes: 'border-t border-gray-200 text-center mx-2 py-6 lg:py-8',
+      classes: 'flex justify-center border-t border-gray-200 mx-2 py-6 lg:py-8',
       [
-        a(
-          classes:
-              'decoration-2 decoration-sky-500 underline underline-offset-8 mx-auto text-sm',
-          href: 'https://github.com/schultek/jaspr',
-          target: Target.blank,
-          [
-            text('본 홈페이지는 '),
-            span(classes: 'font-bold', [text('Jaspr')]),
-            text('를 사용해 제작되었습니다.'),
-          ],
-        ),
+        const JasprBadge.darkTwoTone(),
       ],
     );
   }
